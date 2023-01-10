75704a46194816ef22fbde53b2435c68f88b55fc
SolomonVP <Lum7023@yandex.ru>
2023-01-10 23:29:09 +0300

    add, delete, update

--- src/OwnList.java
+++ src/OwnList.java
@@ -1,4 +1,32 @@
 
 public class OwnList<T> {
+    private final int capacity = 10;
+    int up = 2;
+    int count = 0;
+    Object [] objectList = new Object[capacity];
 
+    public void add(T value) {
+        if (objectList.length-1 == count) {
+            overwrite();
+        }
+        objectList[count++] = value;
+    }
+    public void delete(int index) {
+        objectList[index] = null;
+    }
+
+    public void update() {
+
+    }
+
+    public T get(int index) {
+        return (T) objectList[index];
+    }
+
+    private void overwrite() {
+        Object [] newObjectList = new Object[objectList.length * 2];
+//        System.arrayCopy(from, fromIndex, to, toIndex, count) - copy array;
+        System.arraycopy(objectList, 0, newObjectList, 0, count - 1);
+        objectList = newObjectList;
+    }
 }
