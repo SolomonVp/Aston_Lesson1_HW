42f965be65ddc4645065daf182764b5d98c1cfd1
SolomonVP <Lum7023@yandex.ru>
2023-01-12 15:22:58 +0300

    bubbleSort

--- src/OwnList.java
+++ src/OwnList.java
@@ -1,55 +1,63 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class OwnList<T>{
     private final int capacity = 10;
     private int count = 0;
-    Object [] arr = new Object[capacity];
+    Object [] ownList = new Object[capacity];
 
     public void add(T value) {
-        if (arr.length == count) {
+        if (ownList.length == count) {
             overwrite();
         }
-        arr[count++] = value;
+        ownList[count++] = value;
     }
     public T get(int index) {
-        return (T) arr[index];
+        return (T) ownList[index];
     }
     public int size() {
         return count;
     }
     private void overwrite() {
-        Object [] newObjectList = new Object[arr.length * 2];
-//        System.arrayCopy(from, fromIndex, to, toIndex, count) - copy array;
-        System.arraycopy(arr, 0, newObjectList, 0, arr.length);
-        arr = newObjectList;
+        Object [] newObjectList = new Object[ownList.length * 2];
+        System.arraycopy(ownList, 0, newObjectList, 0, ownList.length);
+        ownList = newObjectList;
     }
-
     public void delete(int index) {
         for (int i = index; i < count; i++) {
-            arr[i] = arr[i + 1];
+            ownList[i] = ownList[i + 1];
         }
     }
 
-    public void addAll(OwnList<T> ownList2) {
-        Object [] l = new Object[arr.length + ownList2.size()];
-//        if (arr.length + ownList2.size() > count) {
-//            overwrite();
-//        }
-        System.arraycopy(arr, 0, l, arr.length, ownList2.size());
-        arr = l;
+    public void set(int index, int value) {
+        ownList[index] = value;
     }
 
-
-//    public void bubbleSort(int [] arr) {
-//        boolean isSorted = false;
-//        while (!isSorted) {
-//            isSorted = true;
-//            for (int i = 1; i < arr.length; i++) {
-//                if (arr[i] < arr[i - 1]) {
-//                    int s = arr[i];
-//                    arr[i] = arr[i - 1];
-//                    arr[i - 1] = s;
-//                    isSorted = false;
-//                }
-//            }
+//    public void addAll(OwnList<? extends T> ownList, OwnList<? extends T> ownList2) {
+//        OwnList <? extends T> ownList3 = new OwnList<>();
+//
+////        for (int i = 0; i < ownList.size(); i++ ) {
+////            ownList3[i] = ownList[i];
+////        }
+//
+//
+//        int [] arr1 = new int[ownList.size()];
+//        int [] arr2 = new int[ownList2.size()];
+//        int [] arr3 = new int[ownList.size() + ownList2.size()];
+//        for (int i = 0; i < ownList.size(); i++) {
+//            arr1[i] = (int) ownList.get(i);
+//        }
+//        for (int i = 0; i < ownList2.size(); i++) {
+//            arr2[i] = (int) ownList2.get(i);
+//        }
+//
+//        System.arraycopy(arr3, 0, arr1, 0, arr1.length);
+//        System.arraycopy(arr3, arr1.length, arr2, 0, arr2.length);
+//        System.out.println(Arrays.toString(arr3));
+//
+//        for (int i = 0; i < arr3.length; i++) {
+//            ownList3.add(arr3[i]);
 //        }
 //    }
 }
