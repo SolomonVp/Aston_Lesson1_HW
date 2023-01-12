6e99705a6adb6963590c034e6f7af476c4b7b322
SolomonVP <Lum7023@yandex.ru>
2023-01-12 11:12:16 +0300

    added method 'delete'

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,45 @@
 //        Плюс написать реализацию сортировки пузырьком с флагом, который прекращает сортировку,
 //        если коллекция уже отсортирована (если arrayList - сортировка в нем, если linked - в отдельном месте)
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         OwnList <Integer> ownList = new OwnList<>();
 
         // + add
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 22; i++) {
             ownList.add(i*2);
             System.out.print(ownList.get(i) + " ");
         }
-        System.out.println("\n--------------------------");
 
         // + size
-        System.out.println(ownList.size() + " - длина");
+        System.out.println("\n" + ownList.size() + " - длина");
 
         // + get
         System.out.println(ownList.get(4) + " - эдемент под индексом");
 
-        // delete
+        // + delete
         ownList.delete(4);
         for (int i = 0; i < ownList.size(); i++) {
             System.out.print(ownList.get(i) + " ");
         }
+        System.out.println("\n" + ownList.size() + " - длина");
+        System.out.println(ownList.get(4) + " - эдемент под индексом");
+
+        // sort
+
+        // ownList.bubbleSort();
+
+        OwnList <Integer> ownList2 = new OwnList<>();
+        ownList2.add(34);
+        ownList2.add(45);
+        ownList.addAll(ownList2);
+        for (int i = 0; i < ownList.size(); i++) {
+            System.out.print(ownList.get(i) + " ");
+        }
+        System.out.println("\n" + ownList.size() + " - длина");
     }
 }
 
--- src/OwnList.java
+++ src/OwnList.java
@@ -1,46 +1,55 @@
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.function.Consumer;
-
 public class OwnList<T>{
     private final int capacity = 10;
     private int count = 0;
-    Object [] objectList = new Object[capacity];
+    Object [] arr = new Object[capacity];
 
     public void add(T value) {
-        if (objectList.length == count) {
+        if (arr.length == count) {
             overwrite();
         }
-        objectList[count++] = value;
+        arr[count++] = value;
     }
     public T get(int index) {
-        return (T) objectList[index];
+        return (T) arr[index];
     }
     public int size() {
         return count;
     }
     private void overwrite() {
-        Object [] newObjectList = new Object[objectList.length * 2];
+        Object [] newObjectList = new Object[arr.length * 2];
 //        System.arrayCopy(from, fromIndex, to, toIndex, count) - copy array;
-        System.arraycopy(objectList, 0, newObjectList, 0, count - 1);
-        objectList = newObjectList;
+        System.arraycopy(arr, 0, newObjectList, 0, arr.length);
+        arr = newObjectList;
     }
 
     public void delete(int index) {
-        objectList[index] = null;
-//        if (objectList.length < capacity && count < objectList.length) {
-//            overwrite(objectList.length/2);
+        for (int i = index; i < count; i++) {
+            arr[i] = arr[i + 1];
+        }
+    }
+
+    public void addAll(OwnList<T> ownList2) {
+        Object [] l = new Object[arr.length + ownList2.size()];
+//        if (arr.length + ownList2.size() > count) {
+//            overwrite();
 //        }
-        count--;
-    }
-
-    public void addAll() {
-
+        System.arraycopy(arr, 0, l, arr.length, ownList2.size());
+        arr = l;
     }
 
 
-
-
-
-
+//    public void bubbleSort(int [] arr) {
+//        boolean isSorted = false;
+//        while (!isSorted) {
+//            isSorted = true;
+//            for (int i = 1; i < arr.length; i++) {
+//                if (arr[i] < arr[i - 1]) {
+//                    int s = arr[i];
+//                    arr[i] = arr[i - 1];
+//                    arr[i - 1] = s;
+//                    isSorted = false;
+//                }
+//            }
+//        }
+//    }
 }
